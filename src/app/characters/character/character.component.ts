73c6241aacc86563a635dc17b98257cee6efc2d8
sifloz <hecn@live.com.mx>
2021-05-04 14:03:45 -0500

    Created first character list display using real data

--- src/app/characters/character/character.component.ts
+++ src/app/characters/character/character.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html'
 })
 export class CharacterComponent {
-
+  @Input() character: Character;
+  borderCommonClass: string = "flex flex-row justify-center items-center p-2 my-5 rounded-full border-2";
+  statusCommonClass: string = "w-2 h-2 max-w-2 max-h-2 rounded-full mr-1";
 }
---
+++ src/app/characters/interfaces/character.interface.ts
@@ -0,0 +1,24 @@
+interface CharacterOrigin {
+  name: string;
+  url: string;
+}
+
+interface CharacterLocation {
+  name: string;
+  url: string;
+}
+
+export interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: CharacterOrigin;
+  location: CharacterLocation;
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
